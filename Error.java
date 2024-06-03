3c37d49551237e090236e53e675079932f0a27b2
ashu731810 <ashish.gautam.254681@gmail.com>
2024-06-04 00:39:47 +0530

    Exception code
    this is exception handling program in java

---
+++ Error.java
@@ -0,0 +1,21 @@
+package com.exception;
+
+public class Error {
+
+	public static void main(String[] args) {
+		
+		int [] arr= new int[1000000000];
+		                           //java.lang.OutOfMemoryError
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
