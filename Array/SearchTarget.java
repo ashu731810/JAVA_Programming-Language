7821e3943e86ced8b470f459b190adf2a3bb62b2
Ashish Gautam <ashish.gautam.254681@gmail.com>
2024-06-10 16:28:54 +0530

    searching element (target element)

---
+++ Array/SearchTarget.java
@@ -0,0 +1,24 @@
+
+public class SearchTarget {
+
+	public static void main(String[] args) {
+		int[] arr= {1,3,8,5,7};
+           int target=8;
+           for(int i=0; i<=arr.length-1; i++) 
+           {
+        	   if(target==arr[i]) 
+        	   
+           	   
+        	   {
+        	   System.out.println("target element is="+i);
+           
+        	   }
+           
+           else 
+           
+System.out.println("target element is not present ");        	   
+           }
+	}
+           }
+	
+
